7587da6019af8c0ffd45e9e8d3605d52cb0226e3
devDhruvil <160093248+devDhruvil@users.noreply.github.com>
2025-04-26 17:39:17 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,18 @@
+
+
+first_number =  float(input("Enter the first number: "))
+second_number = float(input("Enter the second number: "))
+
+# Perform operations
+addition = first_number + second_number
+subtraction = first_number - second_number
+multiplication = first_number * second_number
+division = first_number / second_number
+
+# Display results
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
+
+
---
+++ Task2.py
@@ -0,0 +1,4 @@
+firstName = input("Enter the first name: ")
+lastName = input("Enter the last name: ")
+
+print("Hello,"+firstName +" "+ lastName+"! welcome to the python program.")
